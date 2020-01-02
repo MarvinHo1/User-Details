48720064a41b0a93e4598772445243d122da648c
Marvin Ho <myuanHo1@gmail.com>
2020-01-02 11:14:20 -0800

    Initiate User-Details

---
+++ dev/js/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+require('../../scss/style.scss');
+
+const App = () => (
+    <div>
+        <h2>User List</h2>
+        <hr />
+        <h2>User Details</h2>
+        <li> hmmmm </li>
+    </div>
+);
+
+export default App;
