efd6c4c8e09925802fe45d12abf969bc23bab8ce
Marvin Ho <myuanHo1@gmail.com>
2020-01-02 14:32:40 -0800

    Create Action and Action creators

--- dev/js/containers/user-list.js
+++ dev/js/containers/user-list.js
@@ -1,17 +1,43 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {selectUser} from '../actions/index';
 
 class UserList extends Component {
+
+  createListItems() {
+    return this.props.users.map((user) => {
+      return (
+        <li 
+          key={user.id}
+          onClick={() => this.props.selectUser(user)}
+        >
+          {user.first} {user.last}
+        </li>
+      )
+    })
+  }
   render() {
     return(
       <ul>
-        <li>doo</li>
-        <li>daa</li>
-        <li>Dee</li>
+        {this.createListItems()}
       </ul>
     )
   }
 }
+//takes a piece of state from the store and sends it to the component as props.
+function mapStateToProps(state){
+  return {
+    users: state.users
+  }
+}
 
-export default UserList;
+
+//line below is to connect our actions to redux the slectUser: selectUser is the actions and the dispatch is used for redux in the backend
+function matchDispatchToProps(dispatch){
+  return bindActionCreators({selectUser: selectUser}, dispatch)
+}
+
+
+//Line below makes the class aware of the of the store and the data by using connect
+export default connect(mapStateToProps, matchDispatchToProps)(UserList);
--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import userReducers from './reducers-users';
+import ActiveUserReducer from './reducer-active-user';
 //import movieList from './listOfMovies' (just an example)
 
 //redux will only all you to work with one big object.  The goal of allReducers is to combine all your objects  into one big object.
 const allReducers = combineReducers({
-  users: userReducers
+  users: userReducers,
+  activeUser: ActiveUserReducer
   // if you have another .js file you can import it to this file and add it to this allReducer function. (example below:)
   //movies: movieList
 });
